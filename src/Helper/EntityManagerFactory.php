9fa3d637a76eb99a41d01be16c9690274ec64022
João Gabriel Cardoso <joaogabrielrcardoso@gmail.com>
2023-05-16 14:37:38 -0300

    Add console output middleware

--- src/Helper/EntityManagerFactory.php
+++ src/Helper/EntityManagerFactory.php
@@ -4,9 +4,13 @@ namespace JGabrielrc\DoctrineCourse\Helper;
 
 use Doctrine\DBAL\DriverManager;
 use Doctrine\DBAL\Exception;
+use Doctrine\DBAL\Logging\Middleware;
 use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\Exception\MissingMappingDriverImplementation;
 use Doctrine\ORM\ORMSetup;
+use Symfony\Component\Console\Logger\ConsoleLogger;
+use Symfony\Component\Console\Output\ConsoleOutput;
+use Symfony\Component\Console\Output\OutputInterface;
 
 
 class EntityManagerFactory
@@ -23,6 +27,15 @@ class EntityManagerFactory
             paths: array(__DIR__ . "/.."),
             isDevMode: true,
         );
+        $config->setMiddlewares([
+            new Middleware(
+                new ConsoleLogger(
+                    new ConsoleOutput(
+                        OutputInterface::VERBOSITY_DEBUG
+                    )
+                )
+            )
+        ]);
 
         $connection = DriverManager::getConnection([
             'dbname' => $env['DATABASE_NAME'],
